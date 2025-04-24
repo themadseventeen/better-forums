372298fdc82ac563381d6249510352bd63a95420
themadseventeen <55276223+themadseventeen@users.noreply.github.com>
2025-04-24 09:04:53 +0200

    Get used IDs from post

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // @description  Small improvements to the War Thunder forums
 // @author       themadseventeen
 // @match        https://forum.warthunder.com/*
+// @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -68,6 +69,104 @@
         return document.querySelector('#current-user > button:nth-child(1) > div:nth-child(1) > img:nth-child(1)').title;
     }
 
+    function get_user_json(url) {
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: url,
+                headers: {
+                    "Accept": "application/json"
+                },
+                onload: function (response) {
+                    try {
+                        const json = JSON.parse(response.responseText);
+                        resolve(json);
+                    } catch (e) {
+                        reject("JSON parse error: " + e);
+                    }
+                },
+                onerror: function (err) {
+                    reject("Request failed: " + err);
+                }
+            });
+        });
+    };
+
+    async function get_op_id(post) {
+        const op_name = post.querySelector('.trigger-user-card').href.split('/').lastObject;
+
+        if (!(op_name in nick_id_map)) {
+            const op_link = post.querySelector('.trigger-user-card').href + ".json";
+            const op_json = await get_user_json(op_link);
+            const op_id = op_json["user"]["custom_fields"]["gaijin_id"];
+            console.log("Getting ID for " + op_name);
+            nick_id_map[op_name] = op_id;
+            GM_setValue("nick_id_map", nick_id_map);
+        }
+        // console.log(op_name + " : " + op_id);
+        return nick_id_map[op_name];
+    }
+
+    function create_performance_button() {
+        const button = document.createElement('button');
+        button.className = 'performance-mark widget-button btn-flat share no-text btn-icon'; // Matches Discourse button styles
+        button.title = "Get performance";
+
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('viewBox', '0 0 24 24'); // Standard Discourse icon size
+        svg.setAttribute('width', '20');  // Adjust as needed
+        svg.setAttribute('height', '20');
+        svg.setAttribute('class', 'fa d-icon'); // Matches Discourse icon styling
+
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttribute('d', 'M12 4.5c-4.97 0-9 5.5-9 7.5s4.03 7.5 9 7.5 9-5.5 9-7.5-4.03-7.5-9-7.5zm0 13c-3.31 0-6-3.58-6-5.5s2.69-5.5 6-5.5 6 3.58 6 5.5-2.69 5.5-6 5.5zm0-9a3.5 3.5 0 100 7 3.5 3.5 0 000-7z');
+
+        svg.appendChild(path);
+
+        button.appendChild(svg);
+
+        return button;
+    }
+
+
+    function add_performance_button(post) {
+        // get_op_id(post);
+        const body = post.querySelector('.topic-body');
+
+        const actionsDiv = body.querySelector('.post-controls .actions');
+        if (!actionsDiv || actionsDiv.querySelector('.performance-mark')) return; // Avoid duplicate buttons
+
+        const button = create_performance_button();
+
+        button.addEventListener('click', async () => {
+            const id = await get_op_id(post);
+
+            // post.querySelector(".username").children[0].innerText += " " + id;
+            // post.classList.add("performance-mark");
+            await add_performance(post);
+
+            if (event) {
+                event.target.blur();
+                event.target.classList.remove('active');
+            }
+
+        });
+
+        actionsDiv.appendChild(button);
+    }
+
+    async function add_performance(post) {
+        if (post.classList.contains("performance-mark"))
+            return;
+        const op_name = post.querySelector('.trigger-user-card').href.split('/').lastObject;
+        if (op_name in nick_id_map) {
+            post.classList.add("performance-mark");
+            const id = await get_op_id(post);
+
+            post.querySelector(".username").children[0].innerText += " " + id;
+        }
+    }
+
     function markReplies(post) {
         const replyingToSpan = post.querySelector('.reply-to-tab span');
         if (replyingToSpan) {
@@ -120,6 +219,8 @@
             markReplies(post);
             markQuotes(post);
             markMentions(post);
+            add_performance(post);
+            add_performance_button(post); // statshark integration
         });
 
     }
@@ -228,6 +329,8 @@
     waitForElm('.d-header-icons').then(() => {
         headerButtonsReady();
     });
+
+    let nick_id_map = GM_getValue("nick_id_map", Object.create(null));
 
     const postObserver = new MutationObserver(forAllPosts);
     const pageObserver = new MutationObserver(observePage);
